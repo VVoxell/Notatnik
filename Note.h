202e5f850ff1482d90dc2ca1100c5ac84a9b9c47
VVoxell <77212600+VVoxell@users.noreply.github.com>
2021-01-20 10:47:04 +0100

    Add files via upload

--- Note.h
+++ Note.h
@@ -4,10 +4,20 @@
 using namespace System;
 
 
-ref class Note
+public ref class Note
 {
 public:
 	String^ nazwa;
 	DateTime czas_utworzenia;
 	DateTime czas_edycji;
+	Note(String^ z)
+	{
+		nazwa = z;
+		czas_utworzenia = DateTime::Now;
+		czas_edycji = DateTime::Now;
+	}
+	~Note()
+	{
+		
+	}
 };
